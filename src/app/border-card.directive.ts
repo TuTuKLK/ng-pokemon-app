ac111d94c4a8a9cfb0af712f587f1680c15f7232
TuTuKLK <geant.vert42@gmail.com>
2022-06-16 09:25:52 +0200

    border hover

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pkmBorderCard]'
+})
+export class BorderCardDirective {
+  constructor(private el: ElementRef) { 
+    this.setBorder('#f5f5f5');
+    this.setHeight(180);
+  }
+
+  @Input('pkmBorderCard') berderColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.berderColor || '#009688');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+
+  }
+
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`
+  }
+
+}
